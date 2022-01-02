dd8aefd829e958154113bdf4b3435cfee147f1a5
Pete Kneller <pommypete@gmail.com>
2022-01-02 09:10:48 +0000

    Adds the simulation of the fuselage fuel tank

--- diagram.js
+++ diagram.js
@@ -3,33 +3,50 @@ window.app = function() {
   const SIM_SCALING_FACTOR = 4; // the frequency of sim loops
   const MAX_SINGLE_ENGINE_FUEL_FLOW = 3200; // lbs / hr
   const FUEL_LINE_COLOUR = '#36d4dc';
+  const MAX_FUSELAGE_FUEL = 514;
 
   /* Runtime */
 
   const state = {
     powerSetting: 0,
     batteryMasterOn: false,
+    fuelFuselage: MAX_FUSELAGE_FUEL,
   };
 
   const throttlePosition = () =>
     document.getElementById('throttle').valueAsNumber;
 
-  const engineCanRun = () => state.batteryMasterOn;
+  const engineCanRun = () => state.batteryMasterOn && state.fuelFuselage > 0;
 
   const engineIsRunning = () => state.powerSetting > 0;
 
+  const updateFuelLevels = () => {
+    const totalEngineFuelFlow = 2 * MAX_SINGLE_ENGINE_FUEL_FLOW * state.powerSetting / 100;
+    const fuelConsumedLastPeriod = totalEngineFuelFlow / (3600 * SIM_SCALING_FACTOR);
+    state.fuelFuselage = Math.max(state.fuelFuselage - fuelConsumedLastPeriod, 0);
+  };
+
   const updateSimulation = () => {
+    // Calculation of fuel flows/levels is retrospective - the fuel used in the
+    // last simulation period will be calculated and deducted in this period to
+    // bring up to date. So this done first, before any decisions or indications
+    // are made based on fuel state.
+    updateFuelLevels();
+
     state.powerSetting = engineCanRun() ? throttlePosition() : 0;
   };
 
-  const renderPump = (pumpName, isOn) => {
-    document.
-      querySelector(`g[name="${pumpName}"] g[name="impeller"] animateTransform`).
+  const renderPump = (pumpName, isOn, hasPressure) => {
+    const impeller = document.querySelector(`g[name="${pumpName}"] g[name="impeller"]`);
+    impeller.style.setProperty('transform-box', 'fill-box');
+    impeller.style.setProperty('transform-origin', 'center');
+    impeller.
+      querySelector('animateTransform').
       setAttribute('to', isOn ? '60' : '0');
 
     document.
       querySelector(`g[name="${pumpName}"] rect[name="background"]`).
-      style.setProperty('fill', isOn ? FUEL_LINE_COLOUR : 'white');
+      style.setProperty('fill', hasPressure ? FUEL_LINE_COLOUR : 'white');
   };
 
   const renderEngines = () => {
@@ -54,8 +71,8 @@ window.app = function() {
       setAttribute('to', engineIsRunning() ? '4' : '0');
 
     // Fuel pumps
-    renderPump('pumpEngineLeft', engineIsRunning());
-    renderPump('pumpEngineRight', engineIsRunning());
+    renderPump('pumpEngineLeft', engineIsRunning(), engineIsRunning());
+    renderPump('pumpEngineRight', engineIsRunning(), engineIsRunning());
 
     // Fuel lines
     document.
@@ -70,7 +87,7 @@ window.app = function() {
       forEach(indicator => {
         indicator.style.setProperty('transform-box', 'fill-box');
         indicator.style.setProperty('transform-origin', 'center');
-        indicator.style.setProperty('transform', `rotate(${indicatorAngularRate * singleEngineFuelFlow / 1000}deg)`);
+        indicator.style.setProperty('transform', `rotate(${ indicatorAngularRate * singleEngineFuelFlow / 1000 }deg)`);
       });
   };
 
@@ -79,10 +96,22 @@ window.app = function() {
     document.querySelector('g[name="switchBatteryOff"]').style.setProperty('display', state.batteryMasterOn ? 'none' : 'inline' );
   };
 
+  const renderFuselageTank = () => {
+    renderPump('pumpFuselage', state.batteryMasterOn, state.batteryMasterOn && state.fuelFuselage > 0);
+
+    const indicator = document.querySelector('g[name="indicatorFuelFuselage"]');
+    indicator.querySelector('text').textContent = `${new String(Math.floor(state.fuelFuselage)).padStart(3, "0")} lbs`;
+
+    const barStyle = indicator.querySelector('rect[name="barGauge"]').style;
+    barStyle.setProperty('transform', `scaleY(${ state.fuelFuselage / MAX_FUSELAGE_FUEL })`);
+    barStyle.setProperty('transform-box', 'fill-box');
+    barStyle.setProperty('transform-origin', 'bottom');
+  };
+
   const renderUI = () => {
     renderEngines();
     renderBatteryMasterSwitch();
-    renderPump('pumpFuselage', state.batteryMasterOn);
+    renderFuselageTank();
   };
 
   const runSimulation = () => {
@@ -97,6 +126,30 @@ window.app = function() {
   const throttleChangedHandler = () => {
   };
 
+  const prepareFuselageFuelChange = () => {
+    const indicatorGroup = document.querySelector('g[name="indicatorFuelFuselage"]');
+    const input =  indicatorGroup.querySelector('input[type="text"]');
+    input.value = Math.floor(state.fuelFuselage);
+    input.style.setProperty('display', 'inline');
+    input.select();
+
+    indicatorGroup.querySelector('text').style.setProperty('display', 'none');
+  };
+
+  const handleFuselageFuelChange = () => {
+    const indicatorGroup = document.querySelector('g[name="indicatorFuelFuselage"]');
+    const textInput = indicatorGroup.querySelector('input[type="text"]');
+    const newLevel = Number.parseInt(textInput.value);
+    if (!!newLevel || newLevel == 0) {
+      state.fuelFuselage = Math.min(Math.max(newLevel, 0), MAX_FUSELAGE_FUEL);
+    }
+
+    textInput.style.setProperty('display', 'none');
+    indicatorGroup.
+      querySelector('text').
+      style.setProperty('display', 'inline');
+  };
+
   /* end event handlers */
 
   /* Initialisation */
@@ -105,6 +158,8 @@ window.app = function() {
     document.getElementById('throttle').oninput = throttleChangedHandler;
     document.querySelector('g[name="switchBatteryOn"]').onclick = () => { state.batteryMasterOn = false; };
     document.querySelector('g[name="switchBatteryOff"]').onclick = () => { state.batteryMasterOn = true; };
+    document.querySelector('g[name="indicatorFuelFuselage"] text').onclick = prepareFuselageFuelChange;
+    document.querySelector('g[name="indicatorFuelFuselage"] input[type="text"]').onblur = handleFuselageFuelChange;
   };
 
   const initSimulation = () => {
